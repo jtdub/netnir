4b820f5af94e1f68f3f7162d1d4db271527f9147
jtdub <amascuba@gmail.com>
2020-08-12 10:18:38 -0500

    utilize netnir built connections, rather than nornir connection plugins

--- netnir/plugins/netmiko.py
+++ netnir/plugins/netmiko.py
@@ -1,6 +1,4 @@
 from nornir.core.task import Task, Result
-from netmiko import ConnectHandler
-from netnir.helpers import device_mapper
 from typing import Any
 
 
@@ -12,25 +10,16 @@ def netmiko_send_commands(task: Task, commands: list(), **kwargs: Any) -> Result
 
     :returns: nornir Result object
     """
-    try:
-        secret = task.host.connection_options["netmiko"]["extras"]["secret"]
-    except KeyError:
-        secret = None
-
-    device_params = {
-        "host": task.host.hostname,
-        "device_type": device_mapper(os_type=task.host.platform, proto="netmiko"),
-        "port": task.host.port,
-        "username": task.host.username,
-        "password": task.host.password,
-        "secret": secret,
-        "ssh_config_file": task.nornir.config.ssh.config_file or None,
-    }
     output = str()
+    conn = task.host.get_connection(
+        connection="netmiko", configuration=task.nornir.config
+    )
 
-    with ConnectHandler(**device_params) as conn:
-        for command in commands:
-            output += conn.send_command(command)
+    if isinstance(commands, str):
+        commands = [commands]
+
+    for command in commands:
+        output += conn.send_command(command)
 
     return Result(host=task.host, result=output)
 
@@ -43,23 +32,11 @@ def netmiko_send_config(task: Task, commands: list(), **kwargs: Any) -> Result:
 
     :returns: nornir Result object
     """
-    try:
-        secret = task.host.connection_options["netmiko"]["extras"]["secret"]
-    except KeyError:
-        secret = None
-
-    device_params = {
-        "host": task.host.hostname,
-        "device_type": device_mapper(os_type=task.host.platform, proto="netmiko"),
-        "port": task.host.port,
-        "username": task.host.username,
-        "password": task.host.password,
-        "secret": secret,
-        "ssh_config_file": task.nornir.config.ssh.config_file or None,
-    }
-
-    with ConnectHandler(**device_params) as conn:
-        output = conn.send_config_set(commands)
-        output += conn.save()
+    output = str()
+    conn = task.host.get_connection(
+        connection="netmiko", configuration=task.nornir.config
+    )
+    output = conn.send_config_set(commands)
+    output += conn.save()
 
     return Result(task.host, result=output)
